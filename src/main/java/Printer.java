5fd24d3d0b88edab023b5094eb1fd2ef9f5b9e57
Nathan <nathankirley@gmail.com>
2020-06-15 17:04:49 +0100

    hasToner passes

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,13 +1,19 @@
 public class Printer {
 
     private int sheets;
+    private int toner;
 
-    public Printer(int sheets){
+    public Printer(int sheets, int toner){
         this.sheets = sheets;
+        this.toner = toner;
     }
 
     public int getSheets(){
         return this.sheets;
+    }
+
+    public int getToner(){
+        return this.toner;
     }
 
     public boolean enoughPaper(int copies, int pages){
