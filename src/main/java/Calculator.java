38b121fd3068a880ad40975e73e080d30492dbd9
Nathan <nathankirley@gmail.com>
2020-06-15 15:48:43 +0100

    canAdd works

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,15 @@
 public class Calculator {
+
+    private int number1;
+    private int number2;
+
+    public Calculator(int number1, int number2){
+        this.number1 = number1;
+        this.number2 = number2;
+    }
+
+    public int add(){
+        return this.number1 + this.number2;
+    }
+
 }
