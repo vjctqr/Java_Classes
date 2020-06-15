8c859471d6ec1a9e2b2ddcce019f2c925633645f
Nathan <nathankirley@gmail.com>
2020-06-15 16:37:58 +0100

    hasSheets passes

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,4 +1,12 @@
 public class Printer {
 
+    private int sheets;
 
+    public Printer(int sheets){
+        this.sheets = sheets;
+    }
+
+    public int getSheets(){
+        return this.sheets;
+    }
 }
