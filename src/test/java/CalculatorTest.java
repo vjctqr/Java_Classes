92173ff15b9b7e934d6b94acb7829d35cf0d741d
Nathan <nathankirley@gmail.com>
2020-06-15 15:41:18 +0100

    canAdd test written

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,22 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CalculatorTest {
+
+    Calculator calculator;
+
+    @Before
+    public void before(){
+        calculator = new Calculator(20, 5);
+    }
+
+    @Test
+    public void canAdd(){
+        assertEquals(25, calculator.add());
+    }
+
+
+
 }
