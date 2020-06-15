22186c91ed3fb191d6612885693024f7b9dc629a
Nathan <nathankirley@gmail.com>
2020-06-15 16:12:28 +0100

    hasVolume test written

---
+++ src/test/java/WaterBottleTest.java
@@ -0,0 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class WaterBottleTest {
+
+    WaterBottle waterBottle;
+
+    @Before
+    public void before(){
+        waterBottle = new WaterBottle(100);
+    }
+
+    @Test
+    public void hasVolume(){
+        assertEquals(100, waterBottle.volume());
+    }
+
+
+}
